ba6e36e8d687afe4d02047618f462bc2a3d8c7c5
Tiberiu Carp <tiberiu.carp@openet.com>
2022-05-13 08:34:53 +0100

    Make a release version.

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -107,7 +107,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.9.7-1-OPENET-SNAPSHOT"
+	chart.Version = "1.9.7-1-OPENET"
 
 	return writeYaml(&chart, chartOutput)
 }
