ed5a3d81cb2ed6b8d98c604d00813edfc7a681be
rafaelb <rafael.barros@openet.com>
2022-09-20 13:25:04 +0100

    remove SNAPSHOT qualifier

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -107,7 +107,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.9.26-OPENET-SNAPSHOT"
+	chart.Version = "1.9.26-OPENET"
 
 	return writeYaml(&chart, chartOutput)
 }
