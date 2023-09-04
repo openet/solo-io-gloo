25d6c263c184ec0431c1a655f444b728d8388846
Aditya Jalkhare <aditya.jalkhare@gmail.com>
2023-09-04 13:53:25 +0530

    1.14.12-OPENET-SNAPSHOT-05

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -108,7 +108,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.14.12-OPENET-SNAPSHOT-04"
+	chart.Version = "1.14.12-OPENET-SNAPSHOT-05"
 
 	return writeYaml(&chart, chartOutput)
 }
