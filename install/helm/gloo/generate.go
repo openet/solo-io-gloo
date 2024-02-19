88554955377b7b5baab07ed1f016820fd522c06f
Tiberiu Carp <tiberiu.carp@openet.com>
2024-02-19 12:19:23 +0000

    Add ignore-not-found in checks of existence of gloo-resource-rollout job.

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -108,7 +108,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.14.28-OPENET-4"
+	chart.Version = "1.14.28-OPENET-5-SNAPSHOT"
 
 	return writeYaml(&chart, chartOutput)
 }
