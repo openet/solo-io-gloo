1b2b1d38387afb3afea60ca87b5e59cc22d202c8
Tiberiu Carp <tiberiu.carp@amdocs.com>
2025-01-13 19:04:50 +0000

    Correction in Helm chart.

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -108,7 +108,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.14.32-OPENET-13-SNAPSHOT"
+	chart.Version = "1.14.32-OPENET-14-SNAPSHOT"
 
 	return writeYaml(&chart, chartOutput)
 }
