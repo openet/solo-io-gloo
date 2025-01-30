aa9ffaca3f61a9e59318e427a67385b67cab1afe
Tiberiu Carp <tiberiu.carp@amdocs.com>
2025-01-30 14:37:12 +0000

    Adding functionality from the post-install job + small fixes.

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -106,7 +106,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.15.34-OPENET-1-SNAPSHOT"
+	chart.Version = "1.15.35-OPENET-SNAPSHOT"
 
 	return writeYaml(&chart, chartOutput)
 }
