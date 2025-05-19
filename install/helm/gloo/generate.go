a8bab2bb8309432ade204ac85bf7403f00d3c9d9
Tiberiu Carp <tiberiu.carp@amdocs.com>
2025-05-20 00:37:05 +0100

    Fix Helm templating error.

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -106,7 +106,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.19.0-OPENET-SNAPSHOT"
+	chart.Version = "1.19.0-OPENET-SNAPSHOT-1"
 
 	return writeYaml(&chart, chartOutput)
 }
