602fdd5ebb878afd0f1f9ea5da23135b793aa6e9
Tiberiu Carp <tiberiu.carp@amdocs.com>
2025-07-25 08:17:18 +0100

    Hardcode to release version.

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -106,7 +106,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.19.0-OPENET"
+	chart.Version = "1.19.4-OPENET"
 
 	return writeYaml(&chart, chartOutput)
 }
