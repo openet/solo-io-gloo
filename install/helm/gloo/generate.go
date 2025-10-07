ce2fde1963da052d641d10944788f786b1065e8c
Tiberiu Carp <tiberiu.carp@amdocs.com>
2025-10-07 15:25:34 +0100

    Porting changes to newly created release branch.

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -106,7 +106,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.20.0-rc4-OPENET-SNAPSHOT"
+	chart.Version = "1.20.1-OPENET-SNAPSHOT"
 
 	return writeYaml(&chart, chartOutput)
 }
