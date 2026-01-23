bd3607b305876c2a0edca2b965aa19781b8a14ed
prasanth-openet <prasanth.prabhakaran@openet.com>
2026-01-23 12:31:50 +0000

    updated chart.Version to 1.20.9-OPENET-SNAPSHOT

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -106,7 +106,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.20.1-OPENET"
+	chart.Version = "1.20.9-OPENET-SNAPSHOT"
 
 	return writeYaml(&chart, chartOutput)
 }
